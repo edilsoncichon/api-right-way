53882b5d10f6255ad0d97b74a22e691ac31f1b02
Edilson Cichon <edilsoncichon@hotmail.com>
2018-12-27 15:22:16 -0200

    Move classes core to Core package.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,12 +33,12 @@ $app->singleton(
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
+    App\Core\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+    App\Core\Exceptions\Handler::class
 );
 
 /*
