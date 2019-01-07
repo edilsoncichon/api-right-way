d2906b4a2def3210d61a7d466569548e167f6532
Edilson Cichon <edilsoncichon@hotmail.com>
2019-01-07 16:07:29 -0200

    Create integration tests for Categories domain.

--- tests/Feature/CategoriesTest.php
+++ tests/Feature/CategoriesTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Domains\Category\Category;
 use App\Domains\User\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\JwtAuthentication;
@@ -9,19 +10,77 @@ use Tests\TestCase;
 
 class CategoriesTest extends TestCase
 {
-    use DatabaseMigrations, JwtAuthentication;
+    use DatabaseMigrations,
+        JwtAuthentication;
 
-    public function test_get_user_not_auth()
+    protected $endpointUri = '/v1/categories';
+
+    public function test_get_with_user_not_auth()
     {
-        $response = $this->get('/v1/categories');
+        $response = $this->get($this->endpointUri);
         $response->assertStatus(401);
     }
 
-    public function test_get_user_auth()
+    public function test_get_categories()
+    {
+        $categories = factory(Category::class, 15)->create();
+        $user = factory(User::class)->create();
+        $data = $this->actingAs($user)
+                     ->get($this->endpointUri)
+                     ->assertOk()->json('data');
+        $this->assertEquals($categories[0]->id, $data[0]['id']);
+    }
+
+    public function test_get_category()
+    {
+        $category = factory(Category::class)->create();
+        $user = factory(User::class)->create();
+        $data = $this->actingAs($user)
+            ->get($this->endpointUri . '/' . $category->id)
+            ->assertOk()
+            ->json('data');
+        $this->assertEquals($category->id, $data['id']);
+        $this->assertEquals($category->name, $data['name']);
+        $this->assertEquals($category->name_slug, $data['name_slug']);
+    }
+
+    public function test_create_category_with_valid_fields()
     {
         $user = factory(User::class)->create();
+        $data = factory(Category::class)->make()->toArray();
         $this->actingAs($user)
-            ->get('/v1/categories')
-            ->assertStatus(200);
+            ->post($this->endpointUri, $data)
+            ->assertOk()
+            ->assertSee('name');
+    }
+
+    public function test_create_category_with_invalid_fields()
+    {
+        $user = factory(User::class)->create();
+        $data = factory(Category::class)->make(['name' => null])->toArray();
+        $this->actingAs($user)
+            ->post($this->endpointUri, $data)
+            ->assertStatus(422);
+    }
+
+    public function test_update_category()
+    {
+        $user = factory(User::class)->create();
+        $data = factory(Category::class)->create()->toArray();
+
+        $this->actingAs($user)
+            ->put($this->endpointUri, $data)
+            ->assertOk()
+            ->assertSee('name');
+    }
+
+    public function test_delete_category()
+    {
+        $user = factory(User::class)->create();
+        $category = factory(Category::class)->create();
+        $this->actingAs($user)
+            ->delete($this->endpointUri . '/' . $category->id)
+            ->assertOk()
+            ->assertSee('user_message');
     }
 }
